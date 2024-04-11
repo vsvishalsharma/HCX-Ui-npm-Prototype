8b87b430bca3cd19d6fe9426f2c78b3d4c2a91f3
Vishal Sharma <vsvishalsharma777@gmail.com>
2024-04-11 17:55:19 +0530

    bug fixes

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default as CustomButton } from './components/CustomButton';
+export { default as DarkModeSwitcher } from './components/DarkModeSwitcher';
+
+// Exporting hooks
+export { default as useColorMode } from './hooks/useColorMode';
+export { default as useDebounce } from './hooks/useDebounce';
+export { default as useLocalStorage } from './hooks/useLocalStorage';
